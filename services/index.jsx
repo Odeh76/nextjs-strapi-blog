58038f67f3be9573bde3d03e7c8c5fa9bd3ee652
Odeh Odeh <aodeh76@gmail.com>
2022-05-14 16:01:17 -0700

    minor fixes

--- services/index.jsx
+++ services/index.jsx
@@ -1,6 +1,6 @@
 import {request, gql} from 'graphql-request'
 
-const graphqlAPI = process.env.NEXT_PUBLIC_STRAPI_ENDPOINT;
+const graphqlAPI = process.env.NEXT_PUBLIC_STRAPI_ENDPOINT || process.env.NEXT_PUBLIC_STRAPI_ENDPOINT_PRODUCTION;
 
 export const getPosts = async () => {
     const query = gql`
