2eb8692d56074af87591252d3e5d195fc989f90b
Odeh Odeh <aodeh76@gmail.com>
2022-06-04 10:45:41 -0700

    backend deployment config

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: 'provider-upload-aws-s3',
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
@@ -10,11 +10,6 @@ module.exports = ({ env }) => ({
         params: {
           Bucket: env('AWS_BUCKET'),
         },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
       },
     },
   },
