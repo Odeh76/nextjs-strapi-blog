aae815d4e878195023e266c2d50ef25f6c2f8a59
Odeh Odeh <aodeh76@gmail.com>
2022-06-28 14:53:44 -0700

    backend development database configuration

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '0.0.0.0'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 1337),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
