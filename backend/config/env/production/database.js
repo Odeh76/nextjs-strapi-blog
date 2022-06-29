f4ca7c310a72156f9867f862dfbd49cc10cf74d4
Odeh Odeh <aodeh76@gmail.com>
2022-06-28 21:03:02 -0700

    backend development database configuration

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,20 +1,17 @@
-const parse = require("pg-connection-string");
-
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
-
 module.exports = ({ env }) => ({
+  connection: {
     client: 'postgres',
     connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
+      host: env('DATABASE_HOST', '0.0.0.0'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
-      debug: false,
+    },
+    debug: false,
   },
 });
