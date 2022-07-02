26df298817f7170a1ce46b1c42a65554ea92294b
Odeh Odeh <aodeh76@gmail.com>
2022-07-01 18:55:43 -0700

    production link added

--- services/index.jsx
+++ services/index.jsx
@@ -1,6 +1,6 @@
 import {request, gql} from 'graphql-request'
 
-const graphqlAPI = process.env.NEXT_PUBLIC_STRAPI_ENDPOINT_PRODUCTION
+const graphqlAPI = process.env.NEXT_PUBLIC_STRAPI_ENDPOINT
 
 export const getPosts = async () => {
     const query = gql`
