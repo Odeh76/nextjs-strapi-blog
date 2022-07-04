4fc0e9a6b22f2e3b0a6e61b1bf3efbabff396bb9
Odeh Odeh <aodeh76@gmail.com>
2022-07-04 14:01:39 -0700

    menu tweaks

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,40 +16,25 @@ const Header = () => {
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='border-b w-full inline-block border-white-400 py-8'>
-            <div className='flex justify-center items-center cursor-pointer font-bold text-xl text-black dark:text-white'>
+            <div className='flex items-center cursor-pointer font-bold text-xl text-black dark:text-white'>
                     <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-                     className='mx-2'>
+                     className=''>
                      {theme === 'light' ? <BsFillMoonFill /> : <BsFillSunFill />}
                     </button>
                 <Link href='/'>
-                    <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black rounded-full px-2'>
+                    <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black rounded-full align-middle text-xl font-semibold cursor-pointer'>
                         Home
                     </span>
                 </Link>
-                <Link href='/about'>
-                    <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black rounded-full px-2'>
-                        About
-                    </span>
-                </Link>
-                <Link href='/projects'>
-                    <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black rounded-full px-2'>
-                        Projects
-                    </span>
-                </Link>
-                <Link href='/blog'>
-                    <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black rounded-full px-2'>
-                        Blog
-                    </span>
-                </Link>
-            {/* <div>
+            <div>
                 {navLinks.map((navLink) => (
                     <Link key={navLink.attributes.slug} href={`/${navLink.attributes.slug}`}>
-                    <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black md:float-right rounded-full px-2 mt-2 align-middle text-2xl ml-4 font-semibold cursor-pointer'>
+                    <span className='mx-2 hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black md:float-right rounded-full text-xl font-semibold cursor-pointer'>
                     {navLink.attributes.Name}
                     </span>
                     </Link>
                     ))}
-                </div> */}
+                </div>
             </div>
         </div>
     </div>
@@ -57,3 +42,18 @@ const Header = () => {
 }
 
 export default Header
+                // <Link href='/about'>
+                //     <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black rounded-full px-2'>
+                //         About
+                //     </span>
+                // </Link>
+                // <Link href='/projects'>
+                //     <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black rounded-full px-2'>
+                //         Projects
+                //     </span>
+                // </Link>
+                // <Link href='/blog'>
+                //     <span className='hover:bg-black hover:text-white dark:hover:bg-slate-50 dark:hover:text-black rounded-full px-2'>
+                //         Blog
+                //     </span>
+                // </Link>
