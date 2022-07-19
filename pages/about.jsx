d9849dc228330c49ce456b915baf9dc76a2bcc07
Odeh Odeh <aodeh76@gmail.com>
2022-07-19 11:48:40 -0700

    minor styling and content migration

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,31 +1,30 @@
 import React from 'react'
-
+import Image from 'next/image'
+import profilepic from '../public/profile.jpg'
 function About() {
   return (
-    <div className='container mx-auto px-10 mb-8 font-semibold text-black dark:text-white'>
+    <div className='container mx-auto px-10 mb-8 font-semibold text-black dark:text-white md:flex gap-8 justify-center items-center'>
+      <div className='flex flex-col max-w-[360px]'>
         <h1 className='text-2xl font-bold'>My name is Abdullah.</h1>
         <p className='text-xl py-8'>
          I am a Web Developer specializing in building modern web applications. I created this portfolio / blog to combine
          my programming and writing hobbies into one space. I got interested in web development in 2018 and have been actively learning about it since.   
         </p>
-        {/* <h1 className='text-2xl font-bold my-2'>Latest tech stack :</h1>
-        <ul className='grid grid-cols-3 col-span-1 max-w-[400px] gap-2 py-2'>
-          <li>React</li>
-          <li>NextJS</li>
-          <li>Headless CMS</li>
-          <li>NodeJS</li>
-          <li>GraphQL</li>
-          <li>Wordpress</li>
-        </ul> */}
-         <div className='text-xl py-8'>
          This blog / portfolio is built with:
-         <div className='grid grid-cols-2 text-lg max-w-[400px] py-2 gap-2'>
+         <div className='grid grid-cols-2 text-lg py-2 gap-2'>
             <a target="_blank" href='https://nextjs.org/'>NextJS</a>
             <a target="_blank" href='https://tailwindcss.com/'>TailwindCSS</a>
             <a target="_blank" href='https://strapi.io/'>Strapi</a>
             <a target="_blank" href='https://graphql.org/'>GraphQL</a> 
          </div>
          </div>
+         <Image
+            width={360}
+            height={360}
+            quality={95}
+            src={profilepic}
+            className="w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+            />
     </div>
   )
 }
